032e4ea4fecbec16fecd957b318984f4471c86b9
Manuel Magni <61920152+manuZX12@users.noreply.github.com>
2022-03-22 15:56:56 -0300

    Exercise 01 completed

---
+++ Assets/Ej01.cs
@@ -0,0 +1,23 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Realizá un programa donde se cree una variable entera llamada num1, que inicialmente valdrá 0. Luego incrementá su valor en 2 y mostralo por pantalla. Después mostrá el  resultado de multiplicarlo por sí mismo. 
+public class Ej01 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int num1 = 0;
+        num1 += 2;
+        Debug.Log(num1);
+        Debug.Log(num1 * num1);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
